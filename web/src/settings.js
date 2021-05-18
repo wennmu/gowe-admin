9f58e17d912a40ac14eb638de13c8104dda373a0
wennmu <492669352@qq.com>
2021-05-18 14:51:21 +0800

    Add web

---
+++ web/src/settings.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  title: 'GoweAdmin',
+
+  showSettings: true,
+  
+  tagsView: true,
+
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
