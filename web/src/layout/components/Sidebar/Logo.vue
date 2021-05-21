a1e5540932b727049f3d235770469a74949d4978
wennmu <492669352@qq.com>
2021-05-21 15:36:28 +0800

    logo

--- web/src/layout/components/Sidebar/Logo.vue
+++ web/src/layout/components/Sidebar/Logo.vue
@@ -2,11 +2,13 @@
   <div class="sidebar-logo-container" :class="{'collapse':collapse}">
     <transition name="sidebarLogoFade">
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <el-avatar v-if="logo" :size="50" :src="logo" class="sidebar-logo"></el-avatar>
+        <!-- <img v-if="logo" :src="logo" class="sidebar-logo"> -->
         <h1 v-else class="sidebar-title">{{ title }} </h1>
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
+       <el-avatar v-if="logo" :size="38" :src="logo" class="sidebar-logo"></el-avatar>
+        <!-- <img v-if="logo" :src="logo" class="sidebar-logo"> -->
         <h1 class="sidebar-title">{{ title }} </h1>
       </router-link>
     </transition>
